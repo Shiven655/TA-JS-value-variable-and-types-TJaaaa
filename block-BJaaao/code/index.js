d995bcdfc395e3c1f2111c94833ac78d49a890b7
Shiven655 <kshiven655@gmail.com>
2021-11-17 11:20:16 +0530

    alert/prompt/

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -16,7 +16,10 @@ let num2 = 32;
 - Alert `The value of num1 is [num1] and the value of num2 is [num2]`
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
-
+let language = "JavaScript"
+alert("I am learning" + language);
+alert("The value of num1 is " + num1 + " and the value of num2 is " + num2);
+alert("the sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
 
@@ -32,7 +35,14 @@ let num2 = 32;
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
 */
-
+/*
+prompt("username")
+prompt("userprofession")
+confirm ("isAdult")
+alert("Iam " + userName + "a" + userprofession);
+console.log("iam" + userName + "a" + userprofession);
+alert ("username");
+*/
 /*
 . Addition using prompt
 
@@ -43,3 +53,10 @@ let num2 = 32;
   - Log using console.log [numA - numB]
   - Log using console.log [numA * numB]
 */
+let numA =10;
+let numB = 20;
+alert("the sum of both numbers" +(numA +numB));
+alert(numA - numB);
+alert (numA * numB);
+console.log (numA + numB);
+console.log (numA * numB);
